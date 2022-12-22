6c9c3650033526f7b2cc01d705867529736bc0c6
Brecht Van der Elst <brechtvanderelst@gmail.com>
2022-12-22 23:00:55 +0100

    init blazor project

---
+++ spotiwood.ui/Spotiwood.UI/Spotiwood.Framework.Authentication/Options/AuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Spotiwood.Framework.Authentication.Options;
+
+public sealed record AuthenticationOptions
+{
+    public string? Authority { get; init; }
+
+    public string? Audience { get; init; }
+
+    public string? ClientId { get; init; }
+
+    public Uri? ApiRoot { get; init; }
+}
