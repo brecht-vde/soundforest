2bbc1e13751b4c57ade19ce851538b7687fd1563
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-03-03 19:50:19 +0100

    #80 try to debug

--- soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
+++ soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
@@ -1,14 +1,25 @@
-﻿using SpotifyAPI.Web;
+﻿using Microsoft.Extensions.Logging;
+using SpotifyAPI.Web;
 
 namespace SoundForest.Exports.Processing.Infrastructure.Exporters;
 internal class SpotifyClientFactory : ISpotifyClientFactory
 {
     private SpotifyClient? _client;
+    private readonly ILogger<SpotifyClientFactory> _logger;
+
+    public SpotifyClientFactory(ILogger<SpotifyClientFactory> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
     public T Create<T>(string token)
     {
         if (_client is null)
             _client = new SpotifyClient(token);
+
+        _logger.LogInformation($"Type: {nameof(T)}");
+        _logger.LogInformation($"SC: {nameof(ISearchClient)}");
+        _logger.LogInformation($"Type == SC?: {nameof(T) == nameof(ISearchClient)}");
 
         return nameof(T) switch
         {
