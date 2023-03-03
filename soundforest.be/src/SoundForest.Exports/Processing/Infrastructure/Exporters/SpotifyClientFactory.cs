dfe3c4328e9114517fa772bd01015b221cb50c0f
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-03-03 20:38:58 +0100

    N80 ensure new client creation

--- soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
+++ soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
@@ -1,28 +1,16 @@
-﻿using Microsoft.Extensions.Logging;
-using SpotifyAPI.Web;
+﻿using SpotifyAPI.Web;
 
 namespace SoundForest.Exports.Processing.Infrastructure.Exporters;
 internal class SpotifyClientFactory : ISpotifyClientFactory
 {
-    private SpotifyClient? _client;
-    private readonly ILogger<SpotifyClientFactory> _logger;
-
-    public SpotifyClientFactory(ILogger<SpotifyClientFactory> logger)
-    {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
-
     // When running on Azure Functions App, type name can't be determined from T, so have to pass type name as parameter
     public T Create<T>(string type, string token)
     {
-        if (_client is null)
-            _client = new SpotifyClient(token);
-
         return type switch
         {
-            nameof(IUserProfileClient) => (T)_client.UserProfile,
-            nameof(IPlaylistsClient) => (T)_client.Playlists,
-            nameof(ISearchClient) => (T)_client.Search,
+            nameof(IUserProfileClient) => (T)new SpotifyClient(token).UserProfile,
+            nameof(IPlaylistsClient) => (T)new SpotifyClient(token).Playlists,
+            nameof(ISearchClient) => (T)new SpotifyClient(token).Search,
             _ => throw new ArgumentOutOfRangeException("Invalid type passed: " + nameof(T))
         };
     }
