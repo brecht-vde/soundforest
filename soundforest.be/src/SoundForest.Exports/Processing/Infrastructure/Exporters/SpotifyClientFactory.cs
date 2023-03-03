ae31e96e6d28d19669cec2e6e6f80dcec758f4f8
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-03-03 19:39:40 +0100

    #80 change syntax because it somehow doesn't work on functions

--- soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
+++ soundforest.be/src/SoundForest.Exports/Processing/Infrastructure/Exporters/SpotifyClientFactory.cs
@@ -10,18 +10,12 @@ internal class SpotifyClientFactory : ISpotifyClientFactory
         if (_client is null)
             _client = new SpotifyClient(token);
 
-        var type = typeof(T);
-
-        switch (type)
+        return nameof(T) switch
         {
-            case Type _ when type is IUserProfileClient:
-                return (T)_client.UserProfile;
-            case Type _ when type is IPlaylistsClient:
-                return (T)_client.Playlists;
-            case Type _ when type is ISearchClient:
-                return (T)_client.Search;
-            default:
-                throw new ArgumentOutOfRangeException("Invalid type passed: " + type?.Name);
-        }
+            nameof(IUserProfileClient) => (T)_client.UserProfile,
+            nameof(IPlaylistsClient) => (T)_client.Playlists,
+            nameof(ISearchClient) => (T)_client.Search,
+            _ => throw new ArgumentOutOfRangeException("Invalid type passed: " + nameof(T))
+        };
     }
 }
